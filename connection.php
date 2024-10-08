791abb2d1641d3220bcb47ee61824f72485530ef
Farro Axza <farroaxza@gmail.com>
2024-10-08 16:27:13 +0700

    refactor(connection) move connection to database to the right file

--- connection.php
+++ connection.php
@@ -27,3 +27,11 @@ if ($result->num_rows > 0) {
         echo "Error creating database: " . $conn->error;
     }
 }
+
+// Connect to the database
+$conn = new mysqli(hostname: $servername, username: $username, password: $password, database: $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
